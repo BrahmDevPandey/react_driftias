c84c224371bc12d3e5ee34ccb2042636187d2460
BrahmDevPandey <brahmdevpandey762@gmail.com>
2022-10-19 22:54:01 +0530

    initial commit of project skeleton

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Homepage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+function Header() {
+  return (
+    <div>
+      <Navbar />
+      This is the header
+    </div>
+  );
+}
+
+export default Header;
