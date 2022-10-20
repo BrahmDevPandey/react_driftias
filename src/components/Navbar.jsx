0e1ecd5102febc76737f59b68278a7204ec09884
BrahmDevPandey <brahmdevpandey762@gmail.com>
2022-10-20 11:31:32 +0530

    added Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,48 @@
 function Navbar() {
-  return <div>This is the navbar</div>;
+  return (
+    <div>
+      <nav className="navbar navbar-expand-md bg-dark navbar-dark py-3 fixed-top">
+        <div className="container">
+          <a href="#" className="navbar-brand">
+            My Portfolio
+          </a>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navmenu"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="navbar-collapse collapse text-center" id="navmenu">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a href="#qualifications" className="nav-link m-auto">
+                  My Qualifications
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#skills" className="nav-link m-auto"></a>
+                My skills
+              </li>
+              <li className="nav-item">
+                <a href="#projects" className="nav-link m-auto">
+                  My Projects
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link m-auto" href="#contact-info">
+                  Contact Info
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
